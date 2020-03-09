e6bfa642eb67e5d6903707c3e698a6da57aae931
Kyle Dove <kyledove@Kyles-MacBook-Air.local>
2020-03-09 05:22:18 -0500

    Engineer class added, engineer class extends employee in terms of properties passed in the array that is used to create the html cards

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+module.exports = class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.role = "Engineer";
+    this.github = github
+  }
+
+  getGithub() {
+    return this.github;
+  }
+
+  getRole(){
+    return this.role;
+  }
+};
