9143d8b8122fb8c3f313c7a83f27ff7155e4d9b2
Kyle Dove <kyledove@Kyles-MacBook-Air.local>
2020-03-09 05:25:27 -0500

    adding intern class, intern extends employee

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+module.exports = class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.role = "Intern";
+    this.school = school
+  }
+
+  getSchool() {
+    return this.school;
+  }
+
+  getRole(){
+    return this.role;
+  }
+};
