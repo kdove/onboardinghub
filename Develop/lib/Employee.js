3d49f796e1c23923744172fa6d679c304e85e895
Kyle Dove <kyledove@Kyles-MacBook-Air.local>
2020-03-09 05:14:19 -0500

    Adding employee class export module

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Employee class
+odule.exports =  class Employee {
+
+    constructor(name, id, email) {
+      this.name = name;
+      this.email = email;
+      this.id = id;    
+    }
+  
+    getName(){
+      return this.name;
+    }
+  
+    getEmail(){
+      return this.email;
+    }
+  
+    getId(){
+      return this.id;
+    }
+  
+    getRole(){
+      return "Employee";
+    }
+  }
